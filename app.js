2b63e217b55a131f9ec3b8413fcb6983eb6abd0d
prze8969 <shewalepranav66@gmail.com>
2026-01-15 01:47:29 +0530

    Second change

--- app.js
+++ app.js
@@ -17,10 +17,13 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 const aiRoutes = require("./routes/ai.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/mybook123";
+
 if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
+
+
+const MONGO_URL = process.env.MONGO_URL;
 
 main()
   .then(() => console.log("connect to DB"))
